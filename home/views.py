a49a529731a3322e913b338a7e705e01a1ab9cc3
omid <omidespargham2001@gmail.com>
2022-09-02 14:09:21 +0430

    first real text added

--- home/views.py
+++ home/views.py
@@ -4,9 +4,9 @@ from django.views import View
 
 class HomeView(View):
     template_name = 'home/index.html'
-
     def get(self, request):
-        return render(request, self.template_name)
+        text = "داشتن بیمه مسافرتی آن‌قدر مهم است که به شما توصیه می‌کنیم، اگر پول تهیه بیمه مسافرتی را ندارید، بهتر است سفر خارج از کشور نروید. اگر اهل سفر و ماجراجویی هستید، خرید بیمه مسافرتی دیگر برایتان یک خدمت لوکس و غیرضروری نیست."
+        return render(request, self.template_name,{"text":text})
 
 
 class ContactUsView(View):
